26368d90183ca944c46caa8a371ac947ee7bc566
Victor Chee <victor.qhj@gmail.com>
2015-07-30 22:08:29 +0800

    custom

---
+++ CustomActivity/CustomActivity/ActionActivity.swift
@@ -0,0 +1,54 @@
+//
+//  ActionActivity.swift
+//  CustomActivity
+//
+//  Created by Victor Chee on 15/7/29.
+//  Copyright (c) 2015年 Victor Chee. All rights reserved.
+//
+
+import UIKit
+
+class ActionActivity: UIActivity {
+    var title: String?
+    var image: UIImage?
+    var url: NSURL?
+    
+    override class func activityCategory() -> UIActivityCategory {
+        return .Action
+    }
+    
+    override func activityType() -> String? {
+        return toString(self)
+    }
+    
+    override func activityTitle() -> String? {
+        return "Action"
+    }
+    
+    override func activityImage() -> UIImage? {
+        return UIImage(named: "logo")
+    }
+    
+    override func canPerformWithActivityItems(activityItems: [AnyObject]) -> Bool {
+        return true
+    }
+    
+    override func prepareWithActivityItems(activityItems: [AnyObject]) {
+        for item in activityItems {
+            if item is String {
+                title = item as? String
+            } else if item is UIImage {
+                image = item as? UIImage
+            } else if item is NSURL {
+                url = item as? NSURL
+            }
+        }
+    }
+    
+    override func performActivity() {
+        print("action: \(title) & \(url)")
+        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+            self.activityDidFinish(true)
+        })
+    }
+}
--- CustomActivity/CustomActivity/ViewController.swift
+++ CustomActivity/CustomActivity/ViewController.swift
@@ -9,12 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var activityViewController: UIActivityViewController!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let shareActivity = ShareActivity()
+        let actionActivity = ActionActivity()
+        activityViewController = UIActivityViewController(activityItems: ["message", NSURL(string: "http://victorchee.github.io")!], applicationActivities: [shareActivity, actionActivity])
+        activityViewController.excludedActivityTypes = [UIActivityTypePrint, UIActivityTypeAssignToContact]
     }
 
+    @IBAction func buttonTapped(sender: UIButton) {
+        self.presentViewController(activityViewController, animated: true, completion: nil)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
